847dbf1791be296a78cebe0c83cd022e6c60c5b8
jknific719 <jknific719@gmail.com>
2017-10-24 00:16:00 -0500

    Edited scripts.js
    Moved location of fuction within file

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-function validateForm() {
-  var n = document.signup.name.value;
-  var e = document.signup.email.value;
-  var a = document.signup.addr.value;
-  var z = document.signup.zip.value;
-  var p = document.signup.phone.value;
-  var b = document.signup.bday.value;
 window.onload = function(){
   document.getElementById("signup").addEventListener('submit', validateForm);
 }
 
+function validateForm(event) {
+var n = document.signup.name.value;
+var e = document.signup.email.value;
+var a = document.signup.addr.value;
+var z = document.signup.zip.value;
+var p = document.signup.phone.value;
+var b = document.signup.bday.value;
   if (n == "") {
     alert("Hey! You forgot to put in your name!")
     return false;
